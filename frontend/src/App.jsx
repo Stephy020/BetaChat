c4e8b84b8780d75366607c5f3cc9dbab9e38bf06
Stephen Abudu <abudustephen72@gmail.com>
2024-11-18 01:23:08 +0100

    Update App.jsx
    chaged the import  state of home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import Home from './pages/home/Home.jsx'
+import Home from './pages/home/home.jsx'
 import Login from './pages/login/login.jsx'
 import Signup from './pages/signup/signup.jsx'
 import "./App.css"
