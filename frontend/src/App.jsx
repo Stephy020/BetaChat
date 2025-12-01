1184633220c8eb9770c3da5a11349bcf3de86898
Stephen Abudu <abudustephen72@gmail.com>
2025-12-01 06:15:45 +0100

    Fix build error: remove duplicate imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import Home from './pages/home/home.jsx'
 import Login from './pages/login/login.jsx'
-import Home from './pages/home/home.jsx'
-import Login from './pages/login/login.jsx'
 import Signup from './pages/signup/signup.jsx'
 import "./App.css"
 import { Navigate, Route, Routes } from 'react-router-dom'
