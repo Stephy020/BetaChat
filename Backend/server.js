976a04e029f5106e702f95add217a4e9d42f4839
Stephen Abudu <abudustephen72@gmail.com>
2025-12-01 02:44:50 +0100

    feat: added ABONIKI AI and feat to upload profile

--- Backend/server.js
+++ Backend/server.js
@@ -41,17 +41,15 @@ app.use("/api/users", userRoutes);
 app.use("/api/polling", pollingRoutes);
 app.use("/api/gemini", geminiRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
 })
 
 
 
-app.get("/", (req, res) =>
-    res.send("server ready")
-);
+
 
 
 server.listen(PORT, () => {
